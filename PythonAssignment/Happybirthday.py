b8340495f6bd20aa52e1fa0b52cbc3fb4ba0611b
ritun <ritun@NOONIA-PC>
2023-12-20 18:21:29 +0000

    Add project files.

---
+++ PythonAssignment/Happybirthday.py
@@ -0,0 +1,10 @@
+import time
+def wishHappyBirthday():
+    print( "Happy birthday to you " )
+    time.sleep( 1 )
+    print( "Happy birthday to you " )
+    time.sleep( 1 )
+    print( "Happy birthday to the flooorr" )
+    time.sleep( 1 )
+
+    print( "Happy birthday you! " )
---
+++ PythonAssignment/main.py
@@ -0,0 +1,3 @@
+
+import Happybirthday
+Happybirthday.wishHappyBirthday()
