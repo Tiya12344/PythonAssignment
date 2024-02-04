6db6496ebfe225d3dedc46cfc07c75359a019fd6
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-02-04 18:02:07 +0000

    i did questions.

---
+++ PythonAssignment/Assignment6.py
@@ -0,0 +1,24 @@
+#Convert 5.5 to int OK it worked
+answer= int(5.5)
+print(answer)
+
+#Convert 30 to float
+answer2 = float(30)
+print(answer2)
+
+#Use concatenation On x = I like and y = python
+x = "i like"
+y = "python"
+#print(x + " " + y)
+print(f"{x} {y}")
+
+#Use repetition on y= python
+print (y*5)
+
+
+
+
+
+#Use range slices 2: 4 on x = I like
+
+print(x[2:4])
