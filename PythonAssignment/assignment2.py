36380af5eb93280eb1a2deaf34c8b84c6b2402ab
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-02-09 17:12:41 +0000

    i did assigment 2

---
+++ PythonAssignment/assignment2.py
@@ -0,0 +1,13 @@
+"""
+ask the person to give a number
+ask them for another number
+take both numbers and subtract
+print the answer
+
+
+
+"""
+num1 = int( input("pick a number "))
+num2 = int( input("pick another number "))
+answer = num1 - num2
+print(answer)
