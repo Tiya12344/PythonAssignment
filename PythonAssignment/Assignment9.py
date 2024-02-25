5f0e0e7091722d20154cae7c4ecb7b8356588b66
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-02-25 17:52:59 +0000

    i did assigment 9

---
+++ PythonAssignment/Assignment9.py
@@ -0,0 +1,15 @@
+x=30
+y=4
+print(f"x*y = {x*y}")
+print(f"x/y = {x/y}")
+print(f"x-y = {x-y}")
+print(f"x+y = {x+y}")
+print(f"x%y = {x%y}")
+
+x=20
+y=10
+print(x==y)
+print(x>=y)
+print(x<=y)
+print(x>y)
+print(x<y)
